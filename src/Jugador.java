68c97e251d192db3863f3206bd23d2f25cd34a32
Mei <mmulleng2526@politecnics.barcelona>
2026-02-24 08:57:58 +0100

    Carregar fitxer .txt + classe Jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,8 +1,44 @@
+import java.util.Date;
+
 /**
  * Classe jugador
  *
  * @author Mei
  * @version 1.0
  */
-public class Jugador {
+public class Jugador extends Persona {
+    //Atributs
+    /**
+     * Dorsal del jugador
+     */
+    private int dorsal;
+    /**
+     * Posicions finals del jugador
+     */
+    final String[] POSICIONS_POSSIBLES = {"POR", "DEF", "MIG", "DAV"};
+    private String posicio;
+    /**
+     * Qualitat del jugador
+     */
+    private double qualitat;
+
+    //Constructor
+
+    /**
+     * Constructor Jugador
+     *
+     * @param nom
+     * @param cognom
+     * @param dataNaixement
+     * @param souAnual
+     * @param dorsal
+     * @param posicio
+     * @param qualitat
+     */
+    public Jugador(String nom, String cognom, Date dataNaixement, double souAnual, int dorsal, String posicio, double qualitat) {
+        super(nom, cognom, dataNaixement, souAnual);
+        this.dorsal = dorsal;
+        this.posicio = posicio;
+        this.qualitat = qualitat;
+    }
 }
