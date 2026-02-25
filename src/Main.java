5a01090e42c40f5e3b9fcd15474891869d9a9581
Abigail <arabalv2526@politecnics.barcelona>
2026-02-25 09:41:48 +0100

    Estract method del main

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("--------------------\n  Football Manager\n--------------------");
-        System.out.println("Ets administrador(1) o gestor d'equips(2)?");
+
+        preguntarUsuario();
+
         int opcio = sc.nextInt();
 
         if (opcio == 1) {
             do {
-                System.out.println("Benvingut al Politècnics Football Manager, Admin.");
-                System.out.println("Escull una opció: ");
-                System.out.println("-----\nMenú:\n-----");
-                System.out.println("1. Veure classificació lliga actual." +
-                        "\n2. Donar d'alta un equip." +
-                        "\n3. Donar d'alta jugador/a o entrenador/a." +
-                        "\n4. Consultar dades del equip." +
-                        "\n5. Consultar dades del jugador/a del equip." +
-                        "\n6. Disputar nova lliga." +
-                        "\n7. Realitzar sessió d'entrenament (del mercat de fitxatges)." +
-                        "\n8. Desar dades dels equips." +
-                        "\n9. Sortir.");
+                mostrarMenu();
                 switch (opcio) {
                     case 1:
 
@@ -31,5 +21,25 @@ public class Main {
 
         }
 
+    }
+
+    private static void preguntarUsuario() {
+        System.out.println("--------------------\n  Football Manager\n--------------------");
+        System.out.println("Ets administrador(1) o gestor d'equips(2)?");
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("Benvingut al Politècnics Football Manager, Admin.");
+        System.out.println("Escull una opció: ");
+        System.out.println("-----\nMenú:\n-----");
+        System.out.println("1. Veure classificació lliga actual." +
+                "\n2. Donar d'alta un equip." +
+                "\n3. Donar d'alta jugador/a o entrenador/a." +
+                "\n4. Consultar dades del equip." +
+                "\n5. Consultar dades del jugador/a del equip." +
+                "\n6. Disputar nova lliga." +
+                "\n7. Realitzar sessió d'entrenament (del mercat de fitxatges)." +
+                "\n8. Desar dades dels equips." +
+                "\n9. Sortir.");
     }
 }
