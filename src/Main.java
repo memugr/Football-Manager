8f2d28c260bf19bd79551cd8fd7d2fe82b9fb80c
Abigail <arabalv2526@politecnics.barcelona>
2026-04-29 09:07:02 +0200

    Saludo a Mei

--- src/Main.java
+++ src/Main.java
@@ -633,6 +633,7 @@ public class Main {
     //----------------------------------------------------------------------------------------------------//
     //GESTOR D'EQUIPS (Mei)
 
+    //Hola, Mei!
     /**
      * Mostra el programa principal del gestor d'equips
      */
